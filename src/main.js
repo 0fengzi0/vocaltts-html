fc61d0e389d504e719124181fc4a2682d8204356
疯子 <1209711408@qq.com>
2020-07-16 11:13:40 +0800

    vuetify自动引入 依赖更新

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify' // path to vuetify export
+import Vuetify from 'vuetify/lib'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify)
 
 new Vue({
-    router,
-    vuetify,
-    render: h => h(App)
+	router,
+	render: h => h(App),
+	vuetify: new Vuetify({
+		icons: {
+			iconfont: 'mdi', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+		},
+		theme: {
+			themes: {
+				dark: true,
+				light: {
+					primary: '#1976D2',
+					secondary: '#424242',
+					accent: '#82B1FF',
+					error: '#FF5252',
+					info: '#2196F3',
+					success: '#4CAF50',
+					warning: '#FFC107',
+				},
+			},
+		},
+	})
 }).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,40 +1,52 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = {
-        publicPath: '',
-        // webpack的相关配置在这里
-        configureWebpack: {
-            plugins: [
-                // Gzip压缩
-                new CompressionPlugin({
-                    algorithm: 'gzip', //'brotliCompress'
-                    test: /\.js$|\.html$|\.css/, // + $|\.svg$|\.png$|\.jpg
-                    threshold: 10240, //对超过10k的数据压缩
-                    deleteOriginalAssets: false //不删除原文件
-                }),
-                new TerserPlugin({
-                    cache: true,
-                    parallel: true,
-                    sourceMap: false,
-                    terserOptions: {
-                        compress: {
-                            drop_console: true,
-                            drop_debugger: true,
-                        },
-                    },
-                }),
-            ]
-        }
-    };
+	module.exports = {
+		publicPath: '',
+		// webpack的相关配置在这里
+		configureWebpack: {
+			plugins: [
+				// Gzip压缩
+				new CompressionPlugin({
+					algorithm: 'gzip', //'brotliCompress'
+					test: /\.js$|\.html$|\.css/, // + $|\.svg$|\.png$|\.jpg
+					threshold: 10240, //对超过10k的数据压缩
+					deleteOriginalAssets: false //不删除原文件
+				}),
+				// 代码清理
+				new TerserPlugin({
+					cache: true,
+					parallel: true,
+					sourceMap: false,
+					terserOptions: {
+						compress: {
+							drop_console: true,
+							drop_debugger: true,
+						},
+					},
+				}),
+				// vuetify自动导入
+				new VuetifyLoaderPlugin(),
+			]
+		}
+	};
 } else {
-    module.exports = {
-        publicPath: '',
+	module.exports = {
+		publicPath: '',
 
-        // 开发环境配置
-        devServer: {
-            disableHostCheck: true,
-        },
-    }
+		// 开发环境配置
+		devServer: {
+			disableHostCheck: true,
+		},
+		// webpack的相关配置在这里
+		configureWebpack: {
+			plugins: [
+				// vuetify自动导入
+				new VuetifyLoaderPlugin(),
+			]
+		}
+	}
 }
